464b7840bcb487f612cf5cbc1b8ea6ea9ec4c2c4
Muhammadjon Abduvahobov <115058559+Megacoderuzb@users.noreply.github.com>
2023-08-07 22:24:38 +0500

    tailwind and mui config finished

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-xl font-bold bg-sky-50">Kalde</h1>;
+      <Button variant="contained">Hello World</Button>
+    </>
+  );
+}
+
+export default App;
