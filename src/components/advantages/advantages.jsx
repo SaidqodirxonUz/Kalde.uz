a7ff54745555e297492981b4e9167016201be68f
SaidqodirxonUz <SaidqodirxonUz@gmail.com>
2023-08-27 17:46:30 +0500

    errors fixed

--- src/components/advantages/advantages.jsx
+++ src/components/advantages/advantages.jsx
@@ -16,6 +16,8 @@ import { FiArrowUpRight } from "react-icons/fi";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
+import { v4 as uuidv4 } from "uuid";
+
 // import { FiArrowUpRight } from "react-icons/fi";
 
 // Наши преимущества
@@ -102,7 +104,7 @@ const Advantages = () => {
               padding: "",
               borderRadius: "0.5em !important",
             }}
-            key={crypto.randomUUID()}
+            key={uuidv4()}
           >
             <CardHeader
               avatar={
