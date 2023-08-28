3ecd954aea6407c43873dafe6d1b00f5a41bdee5
SaidqodirxonUz <SaidqodirxonUz@gmail.com>
2023-08-28 12:13:11 +0500

    update map token

--- src/components/maps/map.jsx
+++ src/components/maps/map.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import ReactMapGL, { Marker } from "react-map-gl";
 
@@ -13,11 +14,14 @@ const Map = () => {
   const mapboxAccessToken =
     "pk.eyJ1IjoibWVnYWNvZGVydXoiLCJhIjoiY2xsdWlpYzl0MTRibjNnbnpzbTd5aXE5aiJ9.6MAIS-HG-samIv5iUBbq0g";
 
+  const mapboxBase64Token =
+    "c2suZXlKMUlqb2liV1ZuWVdOdlpHVnlkWG9pTENKaElqb2lZMnhzZFdwcGNqQXdNV1J5Y0ROcWNIWndZbXBwT1dGc2RpSjkuR2toOG1IY0UwQTk1Nm9mRUNuaGdaUQ==";
+
   return (
     <ReactMapGL
       {...viewport}
       onViewportChange={setViewport}
-      mapboxApiAccessToken={mapboxAccessToken}
+      mapboxApiAccessToken={mapboxBase64Token}
     >
       <Marker latitude={37.7577} longitude={-122.4376}>
         <div className="marker">Your Marker</div>
